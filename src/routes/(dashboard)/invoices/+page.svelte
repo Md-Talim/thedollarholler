3d44b5903edeeba75b595e00c2bb5f1b578687b8
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-26 22:25:07 +0530

    Build the new invoice button

--- src/routes/(dashboard)/invoices/+page.svelte
+++ src/routes/(dashboard)/invoices/+page.svelte
@@ -9,4 +9,10 @@
 <div class="flex items-center justify-between">
   <!-- Search Field -->
   <Search />
+
+  <!-- New Invoice Button -->
+  <button
+    class="relative translate-y-0 whitespace-nowrap rounded-lg bg-lavenderIndigo px-10 py-3 text-xl font-bold text-white shadow-colored transition-all hover:-translate-y-2 hover:shadow-coloredHover"
+    >+ Invoice</button
+  >
 </div>
