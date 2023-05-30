2b483d990344870a914c2be88395fcccb68fd04f
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-30 09:43:47 +0530

    Add global types

---
+++ src/global.d.ts
@@ -0,0 +1,34 @@
+import type { ClientStatus, invoiceStatus } from './enums';
+
+interface Invoice {
+  client: Client;
+  createdAt: string;
+  dueDate: string;
+  id: string;
+  invoiceNumber: string;
+  invoiceStatus: invoiceStatus;
+  issueDate: string;
+  lineItems: LineItem[];
+  notes?: string;
+  subject?: string;
+  terms?: string;
+}
+
+interface LineItem {
+  amount: number;
+  description: string;
+  id: string;
+  quantity: number;
+}
+
+interface Client {
+  city: string;
+  clientStatus: ClientStatus;
+  email: string;
+  id: string;
+  name: string;
+  state: string;
+  street: string;
+  street: string;
+  zip: string;
+}
