5418bd6b8f45e211aa745547864793f462491949
Mohammad Talim <talimmohammad116@gmail.com>
2023-09-09 18:48:31 +0530

    Add additional validation for client information

--- src/routes/(dashboard)/invoices/InvoiceForm.svelte
+++ src/routes/(dashboard)/invoices/InvoiceForm.svelte
@@ -46,7 +46,7 @@
     {#if !isNewClient}
       <label for="client">Client</label>
       <div class="flex items-end gap-x-5">
-        <select name="client" id="client">
+        <select name="client" id="client" required={!isNewClient}>
           {#each $clients as client}
             <option value={client.id}>{client.name}</option>
           {/each}
@@ -64,7 +64,7 @@
     {:else}
       <label for="NewClient">New Client</label>
       <div class="flex items-end gap-x-5">
-        <input type="text" name="NewClient" />
+        <input type="text" name="NewClient" id="NewClient" required={isNewClient} />
         <div class="text-base font-bold leading-[3.5rem] text-monsoon">or</div>
         <Button
           label="Existing Client"
@@ -89,7 +89,7 @@
     <div class="field col-span-6 grid gap-x-5" transition:slide>
       <div class="field col-span-6">
         <label for="email">Client's Email</label>
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" required={isNewClient} />
       </div>
 
       <div class="field col-span-6">
