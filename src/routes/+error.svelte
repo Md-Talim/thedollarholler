ec475109d564edb4f5411e6c20c5e5dee0331cdd
Mohammad Talim <talimmohammad116@gmail.com>
2023-09-23 10:09:51 +0530

    Build a custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<svelte:head>
+  <title>Error | The Dollar Holler</title>
+</svelte:head>
+
+<div class="center h-screen w-screen flex-col overflow-x-hidden text-center">
+  <h1
+    class="min-h-[250px] min-w-[430px] bg-error bg-center bg-no-repeat text-[11.25rem] font-black text-daisyBush"
+  >
+    {$page.status}
+  </h1>
+  <div class="relative -top-12">
+    <p class="text-lg text-daisyBush">Sorry!</p>
+    <p class="text-lg text-daisyBush">{$page.error?.message}</p>
+  </div>
+</div>
