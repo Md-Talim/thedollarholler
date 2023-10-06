ddf80b596ebd6403963390cd80e8dbf33d7bc274
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-06 15:57:04 +0530

    Build the forgot password page

---
+++ src/routes/(auth)/forgot-password/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="auth-heading">Forgot Password</h1>
+
+<form>
+  <div class="field">
+    <label for="email" class="text-goldenFizz">Email</label>
+    <input type="email" placeholder="hello@example.com" name="email" id="email" required />
+  </div>
+  <button type="submit" class="auth-button">Send A Reset Email!</button>
+  <p class="mt-4 text-center text-sm text-white">
+    <a href="/login" class="underline hover:no-underline">Ready to login?</a>
+  </p>
+</form>
