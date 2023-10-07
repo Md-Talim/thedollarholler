f20deac5ada64d2d6c9704d34b50b2affb413080
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-07 11:54:09 +0530

    Animate the search input field

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -9,7 +9,26 @@
   <input
     type="text"
     placeholder="Search by Keyword"
-    class="search w-full border-b-2 border-dashed border-pastelPurple bg-transparent font-sansSerif text-lg outline-none focus:border-solid focus:border-lavenderIndigo md:w-64"
+    class="search w-full border-b-2 border-dashed border-pastelPurple bg-transparent pr-20 font-sansSerif text-lg outline-none focus:border-solid focus:border-lavenderIndigo md:w-64 md:pr-0"
   />
-  <button class="font-sansSerif text-xl font-bold text-pastelPurple">Search</button>
+  <button
+    class="pointer-events-none absolute left-8 transform font-sansSerif text-xl font-bold text-pastelPurple transition-transform"
+    >Search</button
+  >
 </div>
+
+<style lang="postcss">
+  ::placeholder {
+    color: transparent;
+  }
+
+  input:not(:placeholder-shown) + button,
+  input:focus + button {
+    @apply pointer-events-auto left-auto right-0 translate-x-[calc(100%-65px)] text-right text-lavenderIndigo md:left-0 md:right-auto md:translate-x-[19rem] lg:translate-x-80;
+  }
+
+  input + button:focus,
+  input:focus + button:hover {
+    @apply text-daisyBush;
+  }
+</style>
