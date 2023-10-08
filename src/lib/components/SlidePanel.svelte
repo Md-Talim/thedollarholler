065bbcc684905a388e878e0933546d31b0701803
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-08 09:55:03 +0530

    Animate the slide out panel and overlay

--- src/lib/components/SlidePanel.svelte
+++ src/lib/components/SlidePanel.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { fly } from 'svelte/transition';
   import { createEventDispatcher } from 'svelte';
   import { Arrow } from './Icons';
   import { Overlay, Portal } from '$lib/components';
@@ -18,6 +19,7 @@
   <Overlay />
   <div
     class="fixed right-0 top-0 z-slidePanel h-screen w-full overflow-y-scroll bg-white px-5 pt-16 lg:w-3/4 lg:px-32 lg:py-20"
+    transition:fly={{ x: 1000, duration: 300 }}
   >
     <button
       class="absolute left-7 top-5 text-pastelPurple hover:text-daisyBush"
